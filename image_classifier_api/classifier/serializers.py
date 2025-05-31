aa58b631203a4c933c843c4175e6b5a6df2f5c59
nhat2605 <101168306+nhat2605@users.noreply.github.com>
2025-05-31 12:22:38 +1000

    Delete image_classifier_api directory

--- image_classifier_api/classifier/serializers.py
+++
@@ -1,4 +0,0 @@
-from rest_framework import serializers
-
-class ImageURLSerializer(serializers.Serializer):
-     image_url = serializers.URLField() 
--- image_classifier_api/image_classifier_api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from classifier import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('classify/', views.ImageClassifierView.as_view()),
-    path('test/', views.TestView.as_view()),
-]
